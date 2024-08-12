5a02a36b69987912a47411d86b60a65e1c3e41f0
asier <asierherrera97@gmail.com>
2024-08-12 13:36:27 +0200

    Inicio segunda version

--- client/src/components/Inicio/Inicio.jsx
+++ client/src/components/Inicio/Inicio.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Inicio.module.css';
 import Typewriter from './Typewriter/Typewriter'; // Asegúrate de que la ruta sea correcta
 
-function App() {
+function Inicio() {
     const texts = [
         "Hello, welcome to my website!",
         "Check out my portfolio.",
@@ -21,13 +21,19 @@ function App() {
                     <Typewriter texts={texts} speed={100} deleteSpeed={50} pause={1000} className={styles.role} />
                 </div>
                 <p className={styles.description}>
-                    Welcome to my portfolio! <span className={styles.introP}>Asier Herrera</span> is an <span className={styles.introP}>Experienced Full Stack Developer</span> with a passion for creating
+                    Welcome to my portfolio! <span className={styles.introP}>Asier Herrera</span> is an <span className={styles.introP}>Experienced </span> <span className={styles.introP}>Full Stack Developer </span> with a passion for creating
                     <span className={styles.introP}> visually stunning</span> and <span className={styles.introP}>intuitive</span> web experiences. With a <span className={styles.introP}>unique</span> blend of technical skills and creative flair,
                     there's <span className={styles.introP}>confidence</span> in the ability to deliver <span className={styles.introP}>custom solutions</span> to various project requirements.
                 </p>
                 <div className={styles.buttons}>
-                    <button className={styles.resumeButton}>Resume</button>
-                    <button className={styles.contactButton}>Contact Me</button>
+                    <button className={styles.cvDownload}>
+                        <i className={`fas fa-cloud-arrow-down ${styles.icon}`}></i>
+                        Download CV
+                    </button>
+                    <button className={styles.contactButton}>
+                        <i className={`fas fa-envelope ${styles.icon}`}></i>
+                        Contact Me
+                    </button>                
                 </div>
             </div>
 
@@ -39,4 +45,4 @@ function App() {
     );
 }
 
-export default App;
+export default Inicio;
--- client/src/components/Inicio/Typewriter/Typewriter.jsx
+++ client/src/components/Inicio/Typewriter/Typewriter.jsx
@@ -1,17 +1,19 @@
-// Typewriter.jsx
 import React, { useState, useEffect } from 'react';
 import styles from './Typewriter.module.css';
 
-const Typewriter = ({ texts, speed = 100, deleteSpeed = 50, pause = 1000 }) => {
+const Typewriter = ({ texts, speed, deleteSpeed, pause }) => {
   const [displayedText, setDisplayedText] = useState('');
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
     const text = texts[currentTextIndex];
     let index = isDeleting ? text.length : 0;
 
     const handleTyping = () => {
+      if (isPaused) return;
+
       if (isDeleting) {
         setDisplayedText(text.substring(0, index));
         index -= 1;
@@ -23,7 +25,11 @@ const Typewriter = ({ texts, speed = 100, deleteSpeed = 50, pause = 1000 }) => {
         setDisplayedText(text.substring(0, index + 1));
         index += 1;
         if (index >= text.length) {
-          setIsDeleting(true);
+          setIsPaused(true);  // Pausa al final de la escritura
+          setTimeout(() => {
+            setIsPaused(false);
+            setIsDeleting(true);
+          }, pause);
         }
       }
     };
@@ -31,20 +37,12 @@ const Typewriter = ({ texts, speed = 100, deleteSpeed = 50, pause = 1000 }) => {
     const intervalId = setInterval(handleTyping, isDeleting ? deleteSpeed : speed);
 
     return () => clearInterval(intervalId);
-  }, [isDeleting, currentTextIndex, texts, speed, deleteSpeed]);
-
-  useEffect(() => {
-    const pauseTimeout = setTimeout(() => {
-      setIsDeleting(true);
-    }, pause);
-
-    return () => clearTimeout(pauseTimeout);
-  }, [displayedText, isDeleting, pause]);
+  }, [isDeleting, currentTextIndex, texts, speed, deleteSpeed, isPaused]);
 
   return (
     <span className={styles.typewriter}>
       {displayedText}
-      <div className={styles.cursor}>|</div>
+      <span className={styles.cursor}>|</span> {/* El cursor ahora está dentro del span */}
     </span>
   );
 };
